2ccfd337b824ea846254505d85528152c9908a7a
erisan akorede <akoredeerisan@gmail.com>
2023-06-29 13:04:26 +0100

    fixed frams chart

--- src/widget/earning/farmList/activityChart.tsx
+++ src/widget/earning/farmList/activityChart.tsx
@@ -3,6 +3,12 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import Typography from "@/components/typography";
 
+var colors = {
+  base: "#0131B4", // blue
+  negative: "#41917F", // red
+  positive: "#D93831", // blue
+};
+
 const BarChart = () => {
   const options = {
     chart: {
@@ -53,6 +59,7 @@ const BarChart = () => {
         borderRadius: 0,
         // Use 'column' instead of 'bar' for vertical bars
         borderColor: "transparent", // Set the border color of the bars to transparent
+
         dataLabels: {
           enabled: false,
         },
@@ -71,38 +78,86 @@ const BarChart = () => {
     series: [
       {
         name: "Volume",
-        data: [631, 727, 400, 400],
-        color: "#67FDB8",
+        data: [631, 607, 400, 750],
+        color: {
+          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+          stops: [
+            [0, "#D45CD7"], // start
+            [0.2, "#D45CD7"], // middle top
+            [0.2, "#67FDB8"], // middle bottom
+            [1, "#67FDB8"], // end
+          ],
+        },
+      },
+      {
+        name: "TVL",
+        data: [589, 209, 500, 300],
+        color: {
+          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+          stops: [
+            [0, "#3FBAE7"], // start
+            [0.2, "#3FBAE7"], // middle top
+            [0.2, "#67FDB8"], // middle bottom
+            [1, "#67FDB8"], // end
+          ],
+        },
+      },
+      {
+        name: "TVL",
+        data: [589, 159, 500, 200],
+        color: {
+          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+          stops: [
+            [0, "#E7C61C"], // start
+            [0.24, "#E7C61C"], // middle top
+            [0.24, "#67FDB8"], // middle bottom
+            [1, "#67FDB8"], // end
+          ],
+        },
+      },
+      {
+        name: "TVL",
+        data: [589, 759, 500, 100],
+        color: {
+          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+          stops: [
+            [0, "#C13B41"], // start
+            [0.3, "#C13B41"], // middle top
+            [0.3, "#67FDB8"], // middle bottom
+            [1, "#67FDB8"], // end
+          ],
+        },
       },
       {
         name: "TVL",
         data: [589, 759, 500, 600],
+        color: {
+          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+          stops: [
+            [0, "#C13B41"], // start
+            [0.2, "#C13B41"], // middle top
+            [0.2, "#67FDB8"], // middle bottom
+            [1, "#67FDB8"], // end
+          ],
+        },
+      },
+      {
+        name: "TVL",
+        data: [589, 659, 500, 200],
         color: "#67FDB8",
       },
       {
         name: "TVL",
-        data: [589, 759, 500, 600],
-        color: "#67FDB8",
-      },
-      {
-        name: "TVL",
-        data: [589, 759, 500, 600],
-        color: "#67FDB8",
-      },
-      {
-        name: "TVL",
-        data: [589, 759, 500, 600],
-        color: "#67FDB8",
-      },
-      {
-        name: "TVL",
-        data: [589, 759, 500, 600],
-        color: "#67FDB8",
-      },
-      {
-        name: "TVL",
-        data: [589, 759, 500, 600],
-        color: "#67FDB8",
+        data: [760, 659, 500, 600],
+        color: {
+          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+          stops: [
+            [0, "#C13B41"], // start
+            [0.1, "#C13B41"], // middle top
+            [0.1, "#67FDB8"], // middle bottom
+            [1, "#67FDB8"], // end
+          ],
+        },
       },
     ],
   };
@@ -110,34 +165,51 @@ const BarChart = () => {
   return (
     <div>
       <div className="flex justify-between items-center mb-5">
-        <div>
+        <div className="flex items-center">
           <Typography
-            className="!font-aeonik-pro !text-[#E5E5E5] !border-r-2 pr-3  !border-r-[#6A6A6A]"
+            className="!font-aeonik-pro !text-[#E5E5E5]"
             variant="body1"
             label="Token Activity"
           />
-        </div>
-        <div className="flex items-center ">
-          <div className="flex items-center 2">
-            <div className="w-[12px] h-[12px] bg-success-3 mr-1"></div>
+          <div className="h-[20px] bg-[#E5E5E5] w-[2px] mx-3"></div>
+          <ul>
             <Typography
-              className="!text-[8px] !font-inter  !text-[#6A6A6A]"
+              className="!font-aeonik-pro !text-[#E5E5E5]"
+              variant="body1"
               label="Volume"
             />
-          </div>
-          <div className="flex items-center mx-3">
-            <div className="w-[12px] h-[12px] bg-[#F2994A] mr-2"></div>
-            <Typography
-              className="!text-[8px] !font-inter !text-[#6A6A6A]"
-              label="TVL"
-            />
-          </div>
-          <div className="bg-[#272336] p-[4px] rounded-[2px]">
-            <Typography
-              variant="body0.5"
-              className="!font-inter !text-[#6A6A6A]"
-              label="30 Days"
-            />
+          </ul>
+        </div>
+        <div className="flex items-center ">
+          <div className="bg-[#151727]  flex items-start justify-between">
+            <div className=" rounded-[2px]">
+              <Typography
+                variant="body0.5"
+                className="!font-inter px-3 py-1 flex items-center !text-[#6A6A6A]"
+                label="D"
+              />
+            </div>
+            <div className="bg-[#272336] rounded-[2px]">
+              <Typography
+                variant="body0.5"
+                className="!font-inter px-3 py-1 flex items-center !text-[#6A6A6A]"
+                label="7"
+              />
+            </div>
+            <div className=" rounded-[2px]">
+              <Typography
+                variant="body0.5"
+                className="!font-inter px-3 py-1 flex items-center !text-[#6A6A6A]"
+                label="30 D"
+              />
+            </div>
+            <div className=" rounded-[2px]">
+              <Typography
+                variant="body0.5"
+                className="!font-inter px-3 py-1 flex items-center !text-[#6A6A6A]"
+                label="6 M"
+              />
+            </div>
           </div>
         </div>
       </div>
